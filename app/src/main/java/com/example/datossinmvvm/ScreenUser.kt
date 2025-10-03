3d04e7ee92bff810cc77b6c7ef225e68caa11c1e
deybithiquise-netizen <deybith.iquise@tecsup.edu.pe>
2025-10-03 16:17:27 -0500

    feat: crear pantalla principal ScreenUser

---
+++ app/src/main/java/com/example/datossinmvvm/ScreenUser.kt
@@ -0,0 +1,107 @@
+package com.example.datossinmvvm
+
+import android.content.Context
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.room.Room
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun ScreenUser() {
+    val context = LocalContext.current
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var dataUser by remember { mutableStateOf("") }
+
+    val db = remember { crearDatabase(context) }
+
+    val dao = db.userDao()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Spacer(Modifier.height(50.dp))
+
+        TextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name: ") },
+            singleLine = true
+        )
+
+        TextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text("Last Name:") },
+            singleLine = true
+        )
+
+        Button(
+            onClick = {
+                val user = User(0, firstName, lastName)
+                coroutineScope.launch {
+                    agregarUsuario(user = user, dao = dao)
+                }
+                firstName = ""
+                lastName = ""
+            }
+        ) {
+            Text("Agregar Usuario", fontSize = 16.sp)
+        }
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    val data = getUsers(dao = dao)
+                    dataUser = data
+                }
+            }
+        ) {
+            Text("Listar Usuarios", fontSize = 16.sp)
+        }
+
+        Text(
+            text = dataUser,
+            fontSize = 20.sp
+        )
+    }
+}
+
+fun crearDatabase(context: Context): UserDatabase {
+    return Room.databaseBuilder(
+        context,
+        UserDatabase::class.java,
+        "user_db"
+    ).build()
+}
+
+
+suspend fun getUsers(dao: UserDao): String {
+    var rpta = ""
+    val users = dao.getAll()
+    users.forEach { user ->
+        val fila = user.firstName + " - " + user.lastName + "\n"
+        rpta += fila
+    }
+    return rpta
+}
+
+suspend fun agregarUsuario(user: User, dao: UserDao) {
+    try {
+        dao.insert(user)
+    } catch (e: Exception) {
+        Log.e("User", "Error: insert: ${e.message}")
+    }
+}
